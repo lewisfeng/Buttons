1f755d6d3e046ecd57fb36a31e398c510009c009
YI BIN FENG <>
2023-12-10 22:20:30 -0800

    project created - init commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by YI BIN FENG on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
