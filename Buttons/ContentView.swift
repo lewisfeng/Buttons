eae85fa761c1ab3992864c6b3ff3bfae280512aa
YI BIN FENG <>
2023-12-12 20:09:25 -0800

    timer A worked

--- Buttons/ContentView.swift
+++ Buttons/ContentView.swift
@@ -8,17 +8,138 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  @State var buttonAText: String = "Timer A    0%"
+  @State var buttonBText: String = "Timer B    0%"
+  
+  @State private var start = false
+  
+  @State private var timer: Timer!
+
+  @State var selectedTimerStartDate: Date?
+  @State var timerAStartDate: Date?
+  @State var timerBStartDate: Date?
+  @State var timerAPercentage: Int = 0
+  @State var timerBPercentage: Int = 0
+  @State var timerAPassedDur: TimeInterval = 0
+  @State var timerADur: Int = 60
+  @State var timerBDur: Int = 90
+  @State var percentage: Int = 0
+  @State var count: Int = 0
+  
+  @State private var selecterTimerDur: Int = 0
+  @State private var selecterTimer: Timer?
+       
+   var body: some View {
+
+     NavigationStack {
+       VStack() {
+         Button(action: {
+           start = true
+           selectedTimerStartDate = timerAStartDate
+           selecterTimerDur = timerADur
+         }) {
+         Text(self.buttonAText)
+           .frame(width: 200, height: 50, alignment: .center)
+           .border(Color.black, width: 1.5)
+           .foregroundColor(.black)
+           .padding()
+         }
+         Button(action: {
+           start = true
+           selectedTimerStartDate = timerBStartDate
+           selecterTimerDur = timerBDur
+         }) {
+         Text(self.buttonBText)
+           .frame(width: 200, height: 50, alignment: .center)
+           .border(Color.black, width: 1.5)
+           .foregroundColor(.black)
+           .padding()
+         }
+       }.onAppear{
+         
+         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { t in
+           
+           if selecterTimerDur == timerADur, let d = timerAStartDate {
+             let timePassed = Int(Date().timeIntervalSince(d) + timerAPassedDur) 
+             timerAPercentage = (timePassed*100/timerADur) >= 100 ? 100 : (timePassed*100/timerADur)
+
+             buttonAText = "Timer A   \(timerAPercentage)%"
+           }
+           if selecterTimerDur == timerBDur, let d = timerBStartDate {
+             let timePassed = Int(Date().timeIntervalSince(d))
+             percentage = (timePassed*100/timerBDur) >= 100 ? 100 : (timePassed*100/timerBDur)
+  
+             buttonBText = "Timer B   \(percentage)%"
+           }
+         })
+         
+       }.navigationDestination(isPresented: $start) {
+         if selectedTimerStartDate == timerAStartDate {
+
+           DetailsView(timer: $selecterTimer, timerDur: $selecterTimerDur, btnText: $buttonAText, selectedTimerStartDate: $timerAStartDate, passedDur: $timerAPassedDur, percentage: $timerAPercentage)
+         } else {
+           
+           
+           
+           DetailsView(timer: $selecterTimer, timerDur: $selecterTimerDur, btnText: $buttonAText, selectedTimerStartDate: $timerAStartDate, passedDur: $timerAPassedDur, percentage: $timerAPercentage)
+         }
+         
+       }
+     }
+      
+   }
 }
 
-#Preview {
-    ContentView()
-}
+struct DetailsView: View {
+  @Environment(\.presentationMode) var presentationMode
+
+  @Binding var timer: Timer?
+  @Binding var timerDur: Int
+  @Binding var btnText: String
+  @Binding var selectedTimerStartDate: Date?
+  @Binding var passedDur: TimeInterval
+  @State var dur: Int = 0
+  @Binding var percentage: Int
+  
+  var body: some View {
+    VStack {
+      Button(action: {
+        print("\nbutton clicked")
+        if selectedTimerStartDate == nil {
+          selectedTimerStartDate = Date()
+
+        } else {
+          passedDur += Date().timeIntervalSince(selectedTimerStartDate!)
+          selectedTimerStartDate = nil
+        }
+
+        
+      }) {
+        Text("Start / Pause")
+          .frame(width: 200, height: 50, alignment: .center)
+          .border(Color.black, width: 1.5)
+          .foregroundColor(.black)
+          .padding()
+      }
+      Text("\(percentage)%")
+        .font(.largeTitle)
+
+      }.onAppear{
+
+        
+      }
+      // hide default back button
+      .navigationBarBackButtonHidden(true)
+      // add a toolbar item because we need to hide the arrow icon
+      .toolbar {
+        ToolbarItem(placement: .topBarLeading) {
+          Button(action: {
+            presentationMode.wrappedValue.dismiss()
+          }) {
+            Label("Back", image: "").foregroundColor(.black)
+          }
+        }
+      }
+    }
+  }
+
