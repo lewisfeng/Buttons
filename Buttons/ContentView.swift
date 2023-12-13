1a035a2ff4c932cdfd8021070edd018c312e721d
YI BIN FENG <>
2023-12-12 21:10:58 -0800

    using NavigationStack instead of using buttons

--- Buttons/ContentView.swift
+++ Buttons/ContentView.swift
@@ -21,6 +21,67 @@ struct ContentView: View {
   @State var timerAPercentage: Int = 0
   @State var timerBPercentage: Int = 0
   @State var timerAPassedDur: TimeInterval = 0
+  @State var timerBPassedDur: TimeInterval = 0
+  @State var timerADur: Int = 60
+  @State var timerBDur: Int = 90
+  @State var percentage: Int = 0
+  @State var count: Int = 0
+  
+  @State private var selecterTimerDur: Int = 0
+  @State private var selecterTimer: Timer?
+  
+  var body: some View {
+    NavigationView {
+      VStack {
+          // Your main content here
+
+        NavigationLink(destination: DetailsView(timer: $timer, timerDur: $timerADur, btnText: $buttonAText, selectedTimerStartDate: $timerAStartDate, passedDur: $timerAPassedDur, percentage: $timerAPercentage)) {
+            // Button or any other view that triggers the navigation
+          Text(self.buttonAText)
+            .frame(width: 200, height: 50, alignment: .center)
+            .border(Color.black, width: 1.5)
+            .foregroundColor(.black)
+            .padding()
+        }
+      }.onAppear{
+        if timer == nil {
+          print("timer started")
+          timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { t in
+            
+            if let d = timerAStartDate {
+              let timePassed = Int(Date().timeIntervalSince(d) + timerAPassedDur)
+              timerAPercentage = (timePassed*100/timerADur) >= 100 ? 100 : (timePassed*100/timerADur)
+              
+              buttonAText = "Timer A   \(timerAPercentage)%"
+            }
+            if let d = timerBStartDate {
+              let timePassed = Int(Date().timeIntervalSince(d))
+              timerBPercentage = (timePassed*100/timerBDur) >= 100 ? 100 : (timePassed*100/timerBDur)
+              
+              buttonBText = "Timer B   \(timerBPercentage)%"
+            }
+          })
+        }
+      }
+    }
+  }
+}
+
+/*struct ContentView: View {
+  @State var buttonAText: String = "Timer A    0%"
+  @State var buttonBText: String = "Timer B    0%"
+  
+  @State private var start = false
+  
+  @State private var timer: Timer?
+
+  @State var selectedTimerStartDate: Date?
+  @State var timerAStartDate: Date?
+  @State var timerBStartDate: Date?
+  @State var timerAPercentage: Int = 0
+  @State var timerBPercentage: Int = 0
+  @State var timerAPassedDur: TimeInterval = 0
+  @State var timerBPassedDur: TimeInterval = 0
   @State var timerADur: Int = 60
   @State var timerBDur: Int = 90
   @State var percentage: Int = 0
@@ -60,23 +121,24 @@ struct ContentView: View {
            print("timer started")
            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { t in
              
-             if selecterTimerDur == timerADur, let d = timerAStartDate {
+             if let d = timerAStartDate {
                let timePassed = Int(Date().timeIntervalSince(d) + timerAPassedDur)
                timerAPercentage = (timePassed*100/timerADur) >= 100 ? 100 : (timePassed*100/timerADur)
 
                buttonAText = "Timer A   \(timerAPercentage)%"
              }
-             if selecterTimerDur == timerBDur, let d = timerBStartDate {
+             if let d = timerBStartDate {
                let timePassed = Int(Date().timeIntervalSince(d))
-               percentage = (timePassed*100/timerBDur) >= 100 ? 100 : (timePassed*100/timerBDur)
+               timerBPercentage = (timePassed*100/timerBDur) >= 100 ? 100 : (timePassed*100/timerBDur)
     
-               buttonBText = "Timer B   \(percentage)%"
+               buttonBText = "Timer B   \(timerBPercentage)%"
              }
            })
          }
 
          
        }.navigationDestination(isPresented: $start) {
+  
          if selectedTimerStartDate == timerAStartDate {
 
            DetailsView(timer: $selecterTimer, timerDur: $selecterTimerDur, btnText: $buttonAText, selectedTimerStartDate: $timerAStartDate, passedDur: $timerAPassedDur, percentage: $timerAPercentage)
@@ -84,7 +146,7 @@ struct ContentView: View {
            
            
            
-           DetailsView(timer: $selecterTimer, timerDur: $selecterTimerDur, btnText: $buttonAText, selectedTimerStartDate: $timerAStartDate, passedDur: $timerAPassedDur, percentage: $timerAPercentage)
+           DetailsView(timer: $selecterTimer, timerDur: $selecterTimerDur, btnText: $buttonBText, selectedTimerStartDate: $timerBStartDate, passedDur: $timerBPassedDur, percentage: $timerBPercentage)
          }
          
        }
@@ -92,6 +154,7 @@ struct ContentView: View {
       
    }
 }
+ */
 
 struct DetailsView: View {
   @Environment(\.presentationMode) var presentationMode
@@ -129,7 +192,7 @@ struct DetailsView: View {
 
       }.onAppear{
 
-        
+        print("DetailsView")
       }
       // hide default back button
       .navigationBarBackButtonHidden(true)
