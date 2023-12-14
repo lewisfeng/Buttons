76f668f9688a33359040bb2acfb9432505c5e495
YI BIN FENG <>
2023-12-13 22:54:39 -0800

    replaced regular timer with timer publisher

--- Buttons/ContentView.swift
+++ Buttons/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
   // task 1
   // main logic
   // this is the only timer that runs as soon as app launches, it  checks every second to update UI, I have 3 date objects for timer A, B and C, once user tapps the start/pause button in details view for example timer A, I will check if the date(timerAStartDate) for timer A if its nil, if ture I will make the current date as timer A start date, if false I will re-set it to nil and save how much time timer A has been running in the past(timerAPassedDur), so if timer A gets re-started I will also add timerAPassedDur to get the total running time for timer A then use it to calculate the percentage of the timer A.
-  @State private var timer: Timer?
+  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
   
   // start date for each timer
   @State private var timerAStartDate: Date?
@@ -44,30 +44,9 @@ struct ContentView: View {
         
         Spacer()
 
+        // timer A
         NavigationLink(destination: DetailsView(selectedTimerStartDate: $timerAStartDate, passedDur: $timerAPassedDur, percentage: $timerAPercentage)) {
-          TimerTextView("Timer A", timerAPercentage)
-        }
-        
-        NavigationLink(destination: DetailsView(selectedTimerStartDate: $timerBStartDate, passedDur: $timerBPassedDur, percentage: $timerBPercentage)) {
-          TimerTextView("Timer B", timerBPercentage)
-        }
-        
-        NavigationLink(destination: DetailsView(selectedTimerStartDate: $timerCStartDate, passedDur: $timerCPassedDur, percentage: $timerCPercentage)) {
-          TimerTextView("Timer C", timerCPercentage)
-        }
-        
-        // added some spacers to move up the 3 timer buttons
-        Spacer()
-        Spacer()
-        Spacer()
-        
-      }
-      .onAppear {
-        // we only want to run the timer once
-        if timer == nil {
-          timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { t in
-            
-            // timer A
+          TimerTextView("Timer A", timerAPercentage).onReceive(timer) { _ in
             if let d = timerAStartDate {
               // get the total running timer for the timer
               let totalRunningTime = Int(Date().timeIntervalSince(d) + timerAPassedDur)
@@ -75,43 +54,71 @@ struct ContentView: View {
               timerAPercentage = (totalRunningTime*100/timerADur) >= 100 ? 100 : (totalRunningTime*100/timerADur)
 
               // task 2: screen darkness level
-              updateScreenDarknessLevel(timerAPercentage: timerAPercentage)
+              updateScreenDarknessLevel()
+              
+              cancelTimer()
             }
-            
-            // timer B
+          }
+        }
+        
+        // timer B
+        NavigationLink(destination: DetailsView(selectedTimerStartDate: $timerBStartDate, passedDur: $timerBPassedDur, percentage: $timerBPercentage)) {
+          TimerTextView("Timer B", timerBPercentage).onReceive(timer) { _ in
             if let d = timerBStartDate {
               let totalRunningTime = Int(Date().timeIntervalSince(d) + timerBPassedDur)
               timerBPercentage = (totalRunningTime*100/timerBDur) >= 100 ? 100 : (totalRunningTime*100/timerBDur)
 
               // task 2: system volume
-              updateSystemVolumeLevel(timerBPercentage: timerBPercentage)
+              updateSystemVolumeLevel()
+              
+              cancelTimer()
             }
-            
-            // timer C
+          }
+        }
+        
+        NavigationLink(destination: DetailsView(selectedTimerStartDate: $timerCStartDate, passedDur: $timerCPassedDur, percentage: $timerCPercentage)) {
+          TimerTextView("Timer C", timerCPercentage).onReceive(timer) { _ in
             if let d = timerCStartDate {
               let totalRunningTime = Int(Date().timeIntervalSince(d) + timerCPassedDur)
               timerCPercentage = (totalRunningTime*100/timerCDur) >= 100 ? 100 : (totalRunningTime*100/timerCDur)
+
+              cancelTimer()
             }
-          })
+          }
         }
+        
+        // added some spacers to move up the 3 timer buttons
+        Spacer()
+        Spacer()
+        Spacer()
       }
     }
   }
-}
+  
+  // onec timer ABC percentage reached 100% we need to cancel the timer
+  private func cancelTimer() {
+    if timerAPercentage == 100, timerBPercentage == 100 && timerCPercentage == 100 {
+      timer.upstream.connect().cancel()
+    }
+  }
+  
+  // task 2 - When A timer greater than 20%, start matching the screen darkness level to timer A
+  private func updateScreenDarknessLevel() {
+    if timerAPercentage > 20 {
+      // FIXME: darkness = 1 - brightness?
+      let darknessLevel = 1 - (Double(timerAPercentage)/100.0 > 1 ? 1 : Double(timerAPercentage)/100.0)
+      UIScreen.main.brightness = darknessLevel
+    }
+  }
 
-
-// task 2 - When A timer greater than 20%, start matching the screen darkness level to timer A
-private func updateScreenDarknessLevel(timerAPercentage: Int) {
-  if timerAPercentage > 20 {
-    // FIXME: darkness = 1 - brightness?
-    let darknessLevel = 1 - (Double(timerAPercentage)/100.0 > 1 ? 1 : Double(timerAPercentage)/100.0)
-    UIScreen.main.brightness = darknessLevel
+  // task 2 - Matching B timer with Volume, system volume percentage = Timer B in seconds / 90s. eg. Timer B runs from 0s to 45s, system volume percentage is from 0% to 50%.
+  private func updateSystemVolumeLevel() {
+    var volume = (Float(timerBPercentage)/100.0) * Float(50)/Float(45)
+    volume = volume > 1 ? 1 : volume
+    MPVolumeView.setVolume(volume)
   }
 }
 
-// task 2 - Matching B timer with Volume, system volume percentage = Timer B in seconds / 90s. eg. Timer B runs from 0s to 45s, system volume percentage is from 0% to 50%.
-private func updateSystemVolumeLevel(timerBPercentage: Int) {
-  var volume = (Float(timerBPercentage)/100.0) * Float(50)/Float(45)
-  volume = volume > 1 ? 1 : volume
-  MPVolumeView.setVolume(volume)
-}
+
+
+
